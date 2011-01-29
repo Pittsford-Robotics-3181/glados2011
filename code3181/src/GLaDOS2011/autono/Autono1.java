e18f1d0494d38242d0c351f266dc1879e869ec43
Avaera8820 <Avaera8820@755cc6ac-5c43-cdbf-669c-b33d5054e45a>
2011-01-29 19:05:47 +0000

    Added dead reckoning to Autono1.

--- code3181/src/GLaDOS2011/autono/Autono1.java
+++ code3181/src/GLaDOS2011/autono/Autono1.java
@@ -1,5 +1,7 @@
 package GLaDOS2011.autono;
 
+import GLaDOS2011.*;
+
 /**
  * Intent: Go for the top-middle peg of the left grid.
  * @author Chris Cheng
@@ -7,7 +9,23 @@ package GLaDOS2011.autono;
 public class Autono1 {
     // <editor-fold defaultstate="collapsed" desc="public static void Autono1.run()">
     public static void run() {
-        AutonoSensors.moveOnLine();
+        if(EnhancedIO.getDigital(11)){
+            AutonoSensors.moveOnLine();
+        } else {
+            double timerValue = Hardware.gameTimer.get();
+            if(timerValue < 1){
+                Hardware.drive.driveAtSpeed(.75, .75);
+                Claw.roll(true, .5);
+            }else if(timerValue < 3){
+                Hardware.drive.driveAtSpeed(0, 0);
+                Lifter.goToHeight(9.0);
+                Claw.stop();
+            }else if(timerValue < 5){
+                Claw.tubeIn_Out(false, .5);
+            }else{
+                Claw.stop();
+            }
+        }
     }
     // </editor-fold>
 }
