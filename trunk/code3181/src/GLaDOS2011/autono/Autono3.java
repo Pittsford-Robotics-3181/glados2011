7635b240ce27a5e4dd5ce29a24b4afa158079d71
Avaera8820 <Avaera8820@755cc6ac-5c43-cdbf-669c-b33d5054e45a>
2011-02-01 00:05:02 +0000

    Autono2 and 3 Dead Reckoning complete.

--- trunk/code3181/src/GLaDOS2011/autono/Autono3.java
+++ trunk/code3181/src/GLaDOS2011/autono/Autono3.java
@@ -1,5 +1,7 @@
 package GLaDOS2011.autono;
 
+import GLaDOS2011.*;
+
 /**
  * Intent: Go for the top-left peg of the right grid.
  * @author Chris Cheng
@@ -7,7 +9,29 @@ package GLaDOS2011.autono;
 public class Autono3 {
     // <editor-fold defaultstate="collapsed" desc="public static void Autono3.run()">
     public static void run() {
-        AutonoSensors.moveOnLine();
+        if(!EnhancedIO.getDigital(11)){
+            AutonoSensors.moveOnLine();
+        } else {
+            double timerValue = Hardware.gameTimer.get();
+            if(timerValue < 1){
+                Hardware.drive.driveAtSpeed(.75, .75);
+                Claw.roll(true, .5);
+            }else if(timerValue < 3){
+                Hardware.drive.driveAtSpeed(0.1, -0.1);
+                Claw.stop();
+            }else if(timerValue < 4){
+                Hardware.drive.driveAtSpeed(.75, .75);
+            }else if(timerValue < 4.5){
+                Hardware.drive.driveAtSpeed(-0.1, 0.1);
+                Lifter.goToHeight(9.0);
+            }else if(timerValue < 5){
+                Lifter.goToHeight(9.0);
+            }else if(timerValue < 7){
+                Claw.tubeIn_Out(false, .5);
+            }else{
+                Claw.stop();
+            }
+        }
     }
     // </editor-fold>
 }
